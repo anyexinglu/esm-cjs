a2118c6581bec2dcd7e599ab5b8574578ed19fc1
anyexinglu <1171009543@qq.com>
2021-10-22 20:03:01 +0800

    simple

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 import express from "express";
 import fs from "fs";
-import path from "path";
 import resolve from "enhanced-resolve";
-// import { fileURLToPath } from "url";
-// import { dirname } from "path";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
 
 import { SRC_DIR, removeFirstSlash } from "./helper.js";
 
@@ -25,7 +19,6 @@ app.get("*", (req, res) => {
     const content = fs.readFileSync("./src/index.html"); // 返回 index.html 文件
     res.status(200).type("html").send(content);
   } else {
-    // const src = path.join(__dirname, "./src");
     myResolve(SRC_DIR, removeFirstSlash(url), (err, urlPath) => {
       if (err) {
         console.log("..err", err);
@@ -36,15 +29,7 @@ app.get("*", (req, res) => {
         .set({ "Content-Type": "application/javascript" })
         .send(content);
     });
-
-    // let urlPath = resolve.sync(SRC_DIR, url);
   }
-  // if (url.includes(".js") || !url.includes(".")) {
-  //   if (!url.includes(".")) {
-  //     // js 文件，假设没有后缀 / 后缀含 .js 的都是 JS 文件
-  //     url += ".js";
-  //   }
-  // }
 });
 
 app.listen(5100);
